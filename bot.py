8b12f11f751cdf48c24b3a000266a688437d8bc1
temka700 <tomskmos@gmail.com>
2025-06-25 09:50:15 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,6 @@ from telebot import types
 TOKEN = "7931894873:AAHn2WYyn-Ix83qm2LPp0oaKEJIHYEIXueg"
 bot = telebot.TeleBot(TOKEN)
 
-# Вопросы и варианты с весами по типам (0 - технарь, 1 - гуманитарий, 2 - социальный)
 questions = [
     {
         'text': 'Что вам ближе?',
@@ -64,7 +63,6 @@ questions = [
     },
 ]
 
-# Вузы и направления по профилям
 universities = {
     0: [("МГТУ им. Баумана", "Факультет информатики и вычислительной техники"),
         ("СПбГПУ", "Факультет прикладной математики и механики"),
@@ -84,28 +82,30 @@ user_data = {}
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    markup = telebot.types.InlineKeyboardMarkup(row_width=5)
-    ages = ['менее 18', '18-22', '23-30', '31+']
-    for age in ages:
-        btn = telebot.types.InlineKeyboardButton(age, callback_data='start_test')
-        markup.add(btn)
-    bot.send_message(message.chat.id, "Привет! Выберите, сколько тебе лет:", reply_markup=markup)
-
     markup = types.InlineKeyboardMarkup(row_width=2)
     btn_test = types.InlineKeyboardButton("Пройти тест", callback_data='start_test')
     btn_result = types.InlineKeyboardButton("Посмотреть результат", callback_data='view_result')
     markup.add(btn_test, btn_result)
     bot.send_message(message.chat.id, "Привет! Выбери действие:", reply_markup=markup)
 
-
-
 @bot.callback_query_handler(func=lambda call: True)
 def callback_handler(call):
     user_id = call.from_user.id
 
     if call.data == 'start_test':
-        user_data[user_id] = {'answers': [], 'current_q': 0}
+        markup = types.InlineKeyboardMarkup(row_width=2)
+        ages = ['менее 18', '18-22', '23-30', '31+']
+        for age in ages:
+            btn = types.InlineKeyboardButton(age, callback_data=f'age_{age}')
+            markup.add(btn)
         bot.answer_callback_query(call.id)
+        bot.send_message(call.message.chat.id, "Выберите, сколько вам лет:", reply_markup=markup)
+
+    elif call.data.startswith('age_'):
+        age = call.data[4:]
+        user_data[user_id] = {'age': age, 'answers': [], 'current_q': 0}
+        bot.answer_callback_query(call.id)
+        bot.send_message(call.message.chat.id, f"Вы выбрали возраст: {age}. Начинаем тест.")
         send_question(call.message.chat.id, user_id)
 
     elif call.data == 'view_result':
@@ -118,7 +118,6 @@ def callback_handler(call):
     elif call.data.startswith('answer_'):
         answer_index = int(call.data.split('_')[1])
         if user_id in user_data:
-            current_q = user_data[user_id]['current_q']
             user_data[user_id]['answers'].append(answer_index)
             user_data[user_id]['current_q'] += 1
             bot.answer_callback_query(call.id)
@@ -126,11 +125,9 @@ def callback_handler(call):
             if user_data[user_id]['current_q'] < len(questions):
                 send_question(call.message.chat.id, user_id)
             else:
-                # Анализ результатов
                 result_profile = analyze_answers(user_data[user_id]['answers'])
                 user_data[user_id]['result'] = result_profile
                 send_final_result(call.message.chat.id, result_profile)
-                # Можно сохранить или очистить данные, если нужно
 
 def send_question(chat_id, user_id):
     q_index = user_data[user_id]['current_q']
@@ -142,15 +139,12 @@ def send_question(chat_id, user_id):
     bot.send_message(chat_id, question['text'], reply_markup=markup)
 
 def analyze_answers(answers):
-    # Суммируем веса по профилям
     scores = [0, 0, 0]
     for q_index, ans_index in enumerate(answers):
         weights = questions[q_index]['options'][ans_index]['weights']
         for i in range(len(scores)):
             scores[i] += weights[i]
-    # Определяем профиль с максимальным баллом
     max_score = max(scores)
-    profiles = ['Технический профиль', 'Гуманитарный профиль', 'Социальный профиль']
     profile_index = scores.index(max_score)
     return profile_index
 
@@ -162,7 +156,6 @@ def send_final_result(chat_id, profile_index):
     }
     bot.send_message(chat_id, profiles_desc[profile_index])
 
-    # Предлагаем вуз и направления
     markup = types.InlineKeyboardMarkup(row_width=1)
     for uni, faculty in universities[profile_index]:
         btn = types.InlineKeyboardButton(f"{uni} - {faculty}", callback_data='university_info')
